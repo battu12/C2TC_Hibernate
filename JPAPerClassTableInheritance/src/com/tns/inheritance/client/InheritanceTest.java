5275669d46cdc63998043f5f06014c7b756c6498
bhara <bhara@bharath-rog>
2022-08-27 12:36:50 +0530

    first commit

---
+++ JPAPerClassTableInheritance/src/com/tns/inheritance/client/InheritanceTest.java
@@ -0,0 +1,52 @@
+package com.tns.inheritance.client;
+
+import java.util.Scanner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.tns.inheritance.entities.Employee;
+import com.tns.inheritance.entities.Manager;
+
+public class InheritanceTest {
+	
+public static void main(String[] args) {
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em = factory.createEntityManager();
+		Scanner sc=new Scanner(System.in);
+		em.getTransaction().begin();
+		
+		//create one employee
+		System.out.println("Enter the name of the employee: ");
+		String ename=sc.next();
+		System.out.println("Enter the salary of the employee: ");
+		int esal=sc.nextInt();
+		
+		Employee employee1 = new Employee();
+		employee1.setName(ename);
+		employee1.setSalary(esal);
+		em.persist(employee1);
+		
+		//create one manager
+		System.out.println("Enter the name of the manager: ");
+		String name=sc.next();
+		System.out.println("Enter the salary of the manager: ");
+		int sal=sc.nextInt();
+		System.out.println("Enter the departntment name of the manager: ");
+		String deptname=sc.next();
+		
+		Manager manager1 = new Manager();
+		manager1.setName(name);
+		manager1.setSalary(sal);
+		manager1.setDepartmentName(deptname);
+		em.persist(manager1);
+		
+		em.getTransaction().commit();
+		
+		System.out.println("Added one employee and manager to database.");
+		
+	}
+
+}
---
+++ JPAPerClassTableInheritance/src/com/tns/inheritance/entities/Employee.java
@@ -0,0 +1,40 @@
+package com.tns.inheritance.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int employeeId;
+	private String name;
+	private int salary;
+	
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+}
